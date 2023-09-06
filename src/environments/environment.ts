aacf121a3a01101ff8ac83367175d6b9079e5290
mataillade <matias.taillade@gmail.com>
2023-09-06 10:12:20 +0200

    setup firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'cours-projet-web',
+    appId: '1:843479132285:web:107b6559d6fe0a645f2914',
+    storageBucket: 'cours-projet-web.appspot.com',
+    apiKey: 'AIzaSyDmyhw0BeYCdsr3NnRfb7z0v4h6LtpxY9E',
+    authDomain: 'cours-projet-web.firebaseapp.com',
+    messagingSenderId: '843479132285',
+  },
+};
